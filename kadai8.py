693d5a5b4d7ad005766acc9ab4b98305424b09d3
saba383810 <saba383810@gmail.com>
2020-01-21 11:50:41 +0900

    例外処理を追加しました。

--- kadai8.py
+++ kadai8.py
@@ -1,3 +1,5 @@
+import time
+
 # クラスの定義
 class CarException(Exception):
     def __init__(self,bv):
@@ -9,6 +11,7 @@ class Car:
         self.__capacity = capacity
         self.__gas = gas
         print("\n車を作成しました。\n")
+        time.sleep(1)
 
     def getCapacity(self):
         return self.__capacity
@@ -19,14 +22,19 @@ class Car:
         if gas <= 0:
             raise(CarException(gas))
         self.__gas += gas
+        time.sleep(1)
         print("\n給油が完了しました。\n")
+        time.sleep(1)
 
     def useGas(self):
         self.__gas -= 10
+        time.sleep(1)
         print("\nあなたはドライブを楽しみ10リットル消費した。\n")
+        time.sleep(1)
 
     def show(self):
         print("定員 : ",self.getCapacity(),"人","\nガソリン量 : ",self.getGas(),"リットル")
+        time.sleep(1)
 
 class Track(Car):
 
@@ -35,19 +43,24 @@ class Track(Car):
         
         self.__burden = 0
         print("\nトラックを作成しました。\n")
+        time.sleep(1)
     
     def addBurden(self):
         self.__burden += 10
+        time.sleep(1)
         print("\n積荷が完了しました。")
+        time.sleep(1)
     
     def getBurden(self):
         return self.__burden
 
     def show(self):
-        print("\n現在の状態は\n  定員 : ",self.getCapacity(),"人","\n  ガソリン量 : ",self.getGas(),"リットル","  積荷の量 :",self.getBurden(),"トン\nです")
+        print("\nトラックの状態:\n\n        定員 : ",self.getCapacity(),"人","\n  ガソリン量 : ",self.getGas(),"リットル","\n    積荷の量 : ",self.getBurden(),"トン")
+        time.sleep(1)
 
 #メイン処理
 print("\nこんにちは！カーステーションにようこそ！\n")
+time.sleep(0.5)
 track = Track(3,10)
 while 1:
 
@@ -64,6 +77,7 @@ while 1:
             print("\n0より小さい値は給油できません。")
             print("給油しようとした値は、",e.badValue,"です。")
             print("0より大きい値を入れてください")
+            time.sleep(1)
 
     elif num == 4:
         track.useGas()
@@ -77,4 +91,5 @@ while 1:
         print("\n          正 し い 数 字 を 入 力 し て く だ さ い ! ")
         
 print("ご利用ありがとうございました。\n")
+time.sleep(1)
     
